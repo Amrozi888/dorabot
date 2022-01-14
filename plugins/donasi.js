7d01892af49c6c486745ec0bb47d94c7f55f239b
games-wabot <97183746+Botwa021@users.noreply.github.com>
2022-01-14 12:02:49 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,17 +1,15 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 ┌〔 Donasi • support 〕
-├ cukup gunakan saia dengan 
-├ bijak 😀
-├
-├ https://saweria.co/Doraemond
+├ Dana [088233832771]
+├ Smartfren [088233832771
+├ Tri [089612698583]
+├ 
 ├
 ├ saya harap kamu menyukai bot 
-├ saya dan jangan lupa untuk 
-├ mensupport saya karena saya 
-├ sangat menyukai Dora :D
+├ saya dan jangan lupa untuk berdonasi
 └────
-`.trim(), '© Doraemond', 'Donasi', '.donasi', m)
+`.trim(), '© Mursid', 'Donasi', '.donasi', m)
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
