3aff0aed3b601e9da73a38c11860505452126f55
games-wabot <97183746+Botwa021@users.noreply.github.com>
2022-01-17 14:52:56 +0700

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,20 +1,25 @@
+
 let handler = m => m
 
-let linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
-handler.before = async function (m, { isAdmin, isBotAdmin }) {
-  if (m.isBaileys && m.fromMe) return true
-  let chat = global.db.data.chats[m.chat]
-  let isGroupLink = linkRegex.exec(m.text)
+let linkRegex = /chat.whatsapp.com/i
 
-  if (chat.antiLink && isGroupLink && !isAdmin && !m.isBaileys && m.isGroup) {
-    let thisGroup = `https://chat.whatsapp.com/${await conn.groupInviteCode(m.chat)}`
-    if (m.text.includes(thisGroup)) throw false // jika link grup itu sendiri gak dikick
-    await this.sendButon(m.chat, `*Link Grup Terdeteksi!*${isBotAdmin ? '' : '\n\nbukan admin jadi gabisa kick t_t'}\n\nKetik *.off antilink* untuk mematikan fitur ini${opts['restrict'] ? '' : '\nketik *#on restrict* supaya bisa kick'}`, watermark, 'Matikan Antilink', ',0 antilink', m)
-    if (global.opts['restrict']) {
-      if (isBotAdmin) this.groupRemove(m.chat, [m.sender])
-    }
-  }
-  return true
+handler.before = function (m, { user, isAdmin, isBotAdmin }) {
+
+  if (m.isBaileys && m.fromMe) throw false
+  let chat = global.db.data.chats[m.chat]
+  let name = this.getName(m.sender)
+  let link = linkRegex.exec(m.text)
+  let aizin = m.text.includes("#izinmin") || m.text.includes("#Izin")
+
+  if (chat.antiLink && link && !aizin) {
+ m.reply(`*「 ANTILINK DETECTED 」*\n\nTerdeteksi *${name}* telah mengirim link group!\n\nMaaf Kamu akan dikick dari grup ini!`)
+   this.groupRemove(m.chat, [m.sender])
+  } else if ( chat.antiLink && link && aizin) {
+  	this.sendButton( m.chat, `KAMU TIDAK AKAN DIKICK KARENA KAMU MEMAKAI TANDA #izinmin`, `AntilinkV2`, `Oke`, `ok`, m)
+  	}
 }
+handler.group = true
+handler.admin = true
+handler.botAdmin = true
 
 module.exports = handler
