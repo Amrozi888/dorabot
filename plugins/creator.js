8085162b6268e5144b90aa71d24b7e11b5aa83cd
Amrozi888 <97621090+Amrozi888@users.noreply.github.com>
2022-02-28 21:07:09 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,10 @@
+const contacts = [
+  ['6285828764046', ' *owner-botynyakamu*'],
+  ['6288233832771', ' *creatorbot*'],
+]
 function handler(m) {
-  m.reply('Chat jika penting, harap tidak spam.')
-  this.sendContact(m.chat, '6288233832771@s.whatsapp.net', 'Mursid (Owner)', m)
+  m.reply('Ada perlu apa kak cari owner?, chat *P* tidak akan di balas')
+  for (let [nomor, nama] of contacts) this.sendContact(m.chat, nomor + '@s.whatsapp.net', nama || conn.getName(nomor + '@s.whatsapp.net'), m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
